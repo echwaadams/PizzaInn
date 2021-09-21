dadb1b30eedb9ce3a5bc531538f84ac30f69d96b
echwaadams <adamsechwa18@gmail.com>
2021-09-21 15:06:52 +0300

    add crust prices

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ var pizzaBasePrice = {
     medium: 900,
     large: 1400,
 };
+
+var crust = {
+    small: {
+        "Pizza Bagels" : 300,
+        "Thin Crust" : 400,
+        "Neapolitan Crust" : 505,
+        "Flatbread" : 350,
+    },
+    medium: {
+        "Pizza Bagels" : 500,
+        "Thin Crust" : 600,
+        "Neapolitan Crust" : 605,
+        "Flatbread" : 850,
+    },
+    large: {
+        "Pizza Bagels" : 700,
+        "Thin Crust" : 800,
+        "Neapolitan Crust" : 705,
+        "Flatbread" : 950,
+    }
+}
